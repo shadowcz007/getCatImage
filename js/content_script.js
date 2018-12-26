8d9d9c964baef28da563200cd95f8ee510b9c3a8
shadowcz <shadow@shadowdeMBP-2.lan>
2018-12-26 08:39:11 +0800

    update

--- js/content_script.js
+++ js/content_script.js
@@ -1,11 +1,12 @@
 
-var images=new ImagesOfHtml();
+var images = new ImagesOfHtml();
+init();
 
-async function init_getCatImage(){
+async function init_getCatImage() {
     return new Promise((resolve, reject) => {
         //console.log(_urls);
         chrome.runtime.sendMessage(
-            { type: 'lingvistov', imgurls: _urls,classNames:_classNames },
+            { type: 'lingvistov', imgurls: _urls, classNames: _classNames },
             function (response) {
                 //console.log('收到来自后台的回复：', response);
                 resolve(response);
@@ -15,7 +16,23 @@ async function init_getCatImage(){
 };
 
 
-async function init(){
-   await images.getImgFromElement();
-   console.log(images.result);
+async function init() {
+    await images.getImgFromElement();
+    console.log(images.result);
+
+    await downloadImg(0);
+
 }
+
+async function downloadImg(_i) {
+    let img = await images.loadImgFromHTTP(images.result.urls[_i]);
+    var ctx = images.createCanvasFromImg(img);
+    images.downloadCanvas(ctx.canvas, images.result.titles[_i]);
+    if (_i + 1 >= images.result.titles.length) {
+        var ls = document.querySelector('.bx_pagination_bottom');
+        var lis = ls.querySelectorAll('li');
+        lis[lis.length - 1].children[0].click();
+    } else {
+        await downloadImg(_i + 1);
+    };
+};
--- js/imagesOfHtml.js
+++ js/imagesOfHtml.js
@@ -1,91 +1,147 @@
 class ImagesOfHtml {
 
-    constructor(_isDev) {
-      this.isDev = !!_isDev;
-      this.result={
-        imgs:[],
-        urls:[],
-        types:[],
-        sizes:[]
-      };
+  constructor(_isDev) {
+    this.isDev = !!_isDev;
+    this.result = {
+      imgs: [],
+      urls: [],
+      types: [],
+      sizes: [],
+      titles: []
     };
-  
-    getImgFromElement() {
-      let that = this;
-      let imgs = document.images;
-  
-      //bug 
-      if (that.isDev) console.log(imgs.length);
-  
+  };
+
+
+  imgLoad(_imgs, callback) {
+    var timer = setInterval(function () {
+      let count = _imgs.length;
+      for (let i = 0; i < _imgs.length; i++) {
+        let img = _imgs[i];
+        if (img.complete) {
+          count -= 1;
+        };
+      };
+
+      if (count == 0) {
+        callback();
+        clearInterval(timer);
+      };
+
+    }, 50);
+  };
+
+ async getImgFromElement() {
+    let that = this;
+    let imgs = document.images;
+    return new Promise((resolve, reject) => {
+
+    if (that.isDev) console.log(imgs.length);
+
+    that.imgLoad(imgs, function () {
+      console.log('加载完毕')
       for (let i = 0; i < imgs.length; i++) {
         let img = imgs[i];
         let w = img.naturalWidth,
           h = img.naturalHeight,
           url = img.src;
-  
+
         if (that.isImgMatch(w, h, url)) {
-  
+          let title = img.nextElementSibling.innerText;
+
           that.pushResult(img, url, "element-image", {
             width: w,
             height: h
-          });
-  
+          }, title);
+
         };
       };
-    };
 
-    pushResult(_img,_url,_type,_size){
-      this.result.imgs.push(_img);
-      this.result.urls.push(_url);
-      this.result.types.push(_type);
-      this.result.sizes.push(_size);
-    };
-  
-    isImgMatch(_w, _h, _url) {
-      let isMatch = !!(_w != 0 & _h != 0 & _w > 10 && !_url.match('.svg'));
-      if (this.isDev) console.log(isMatch);
-      return isMatch;
-    };
-  
-  
-    async loadImgFromHTTP(_url) {
-      //console.log("loadImgFromHTTP:", _url);
-      let that = this;
-  
-      return new Promise((resolve, reject) => {
-  
-        let url = _url;
-        let xhr = new XMLHttpRequest();
-        xhr.responseType = 'arraybuffer';
-        xhr.open("GET", url, true);
-        // console.log(xhr);
-        xhr.send();
-  
-        xhr.onload = async function (e) {
-          // console.log(this.response);
-          var blob = this.response;
-          var base64 = btoa(
-            new Uint8Array(blob)
-              .reduce((data, byte) => data + String.fromCharCode(byte), '')
-          );
-          //btoa(String.fromCharCode.apply(null, new Uint8Array(blob)))
-          var str = 'data:image/png;base64,' + base64;
-          var img = await that.loadImg(str);
-          resolve(img);
-        };
-      });
-  
-    };
-  
-    async loadImg(_url) {
-      return new Promise((resolve, reject) => {
-        let img = new Image();
-        img.src = _url;
-        // console.log(str);
-        img.onload = function () {
-          resolve(img);
-        };
-      });
-    };
-  
+      resolve();
+    });
+  });
+
   };
+
+  pushResult(_img, _url, _type, _size, _title) {
+    this.result.imgs.push(_img);
+    this.result.urls.push(_url);
+    this.result.types.push(_type);
+    this.result.sizes.push(_size);
+    this.result.titles.push(_title);
+  };
+
+  isImgMatch(_w, _h, _url) {
+    let isMatch = !!(_w != 0 & _h != 0 & _w > 800 && !_url.match('.svg'));
+    if (this.isDev) console.log(isMatch);
+    return isMatch;
+  };
+
+
+  async loadImgFromHTTP(_url) {
+    //console.log("loadImgFromHTTP:", _url);
+    let that = this;
+
+    return new Promise((resolve, reject) => {
+
+      let url = _url;
+      let xhr = new XMLHttpRequest();
+      xhr.responseType = 'arraybuffer';
+      xhr.open("GET", url, true);
+      // console.log(xhr);
+      xhr.send();
+
+      xhr.onload = async function (e) {
+        // console.log(this.response);
+        var blob = this.response;
+        var base64 = btoa(
+          new Uint8Array(blob)
+            .reduce((data, byte) => data + String.fromCharCode(byte), '')
+        );
+        //btoa(String.fromCharCode.apply(null, new Uint8Array(blob)))
+        var str = 'data:image/png;base64,' + base64;
+        var img = await that.loadImg(str);
+        resolve(img);
+      };
+    });
+
+  };
+
+  async loadImg(_url) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.src = _url;
+      // console.log(str);
+      img.onload = function () {
+        resolve(img);
+      };
+    });
+  };
+
+
+  createCanvasFromImg(_img) {
+    let w = _img.naturalWidth,
+      h = _img.naturalHeight;
+
+    let c = document.createElement('canvas');
+    c.width = w;
+    c.height = h;
+
+    let ctx = c.getContext('2d');
+    ctx.drawImage(_img, 0, 0, w, h);
+
+    return ctx
+
+  };
+
+  downloadCanvas(_canvas, _fileName) {
+
+    var img = _canvas.toDataURL();
+
+    var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+    save_link.href = img;
+    save_link.download = _fileName;
+    save_link.click();
+  };
+
+
+};
